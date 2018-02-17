cee8a7179433be46ef94280a45ae1e82257edfe9
Jurij Zahn <jura.zahn@gmail.com>
2018-02-17 12:24:25 +0100

    Added Constants to declare config key names

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,15 @@
+export const SERVICE = 'serviceConfig';
 export interface IServiceConfig {
   servicePort: number;
 }
 
+export const LOGGING = 'logging';
 export interface ILoggingConfig {
   level: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly';
+}
+
+export const MONGODB = 'mongoDb';
+export interface IMongoConfig {
+  url: string;
+  dbName: string;
 }
