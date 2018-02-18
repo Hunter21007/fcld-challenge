46bd7bd75cf428d79333269be867d254c45491ab
Jurij Zahn <jura.zahn@gmail.com>
2018-02-18 18:46:17 +0100

    Added more apidocs to HealthController

--- src/routes/HeathController.ts
+++ src/routes/HeathController.ts
@@ -5,6 +5,40 @@ import * as winston from 'winston';
 import * as config from 'config';
 import { wrap } from '../utils/express';
 
+/**
+ * This controller provides endpoints to check the app for health
+ * Optionally this class can be extended with metrics insights
+ *
+ * @swagger
+ * definitions:
+ *   AppInfo:
+ *      description: contains app info data
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *        version:
+ *          description: the actual version number of the app
+ *          type: string
+ *        date:
+ *          description: date when the app was built
+ *          type: string
+ *
+ *   HealthInfo:
+ *      type: object
+ *      properties:
+ *        appInfo:
+ *          $ref: '#/definitions/AppInfo'
+ *        env:
+ *          description: the value of the NODE_ENV variable
+ *          type: string
+ *        date:
+ *          description: the current date as iso string of the app
+ *          type: string
+ *
+ * @export
+ * @class HealthController
+ */
 export default class HealthController {
   appInfo: any;
   _log: winston.LoggerInstance;
@@ -14,35 +48,17 @@ export default class HealthController {
     this.appInfo = config.get('appInfo');
   }
 
+  /**
+   * Adds controller endpoints to the express pipeline
+   *
+   * @param {express.Express} app instance of express
+   * @memberof HealthController
+   */
   map(app: express.Express) {
     // This is used to get basic service info and also
     // tho perform health checks on the service
     this._log.info('Mapping health endpoint');
 
-    /**
-     * @swagger
-     * definitions:
-     *   AppInfo:
-     *      description: contains app info data
-     *      type: object
-     *      properties:
-     *        name:
-     *          type: string
-     *        version:
-     *          type: string
-     *        date:
-     *          type: string
-     *
-     *   HealthInfo:
-     *      type: object
-     *      properties:
-     *        appInfo:
-     *          $ref: '#/definitions/AppInfo'
-     *        env:
-     *          type: string
-     *        date:
-     *          type: string
-     */
     /**
      * @swagger
      * /health:
